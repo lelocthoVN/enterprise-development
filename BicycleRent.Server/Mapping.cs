30e0923f41f56156cd0fff93174bbcde2916b0dd
THO LE LOC <leloctho2307@gmail.com>
2024-11-01 12:05:01 +0400

    Добавлен комментарий XML

--- BicycleRent.Server/Mapping.cs
+++ BicycleRent.Server/Mapping.cs
@@ -4,8 +4,14 @@ using BicycleRent.Server.Dto;
 
 namespace BicycleRent.Server;
 
+/// <summary>
+/// Configures mappings between domain models and DTOs
+/// </summary>
 public class Mapping : Profile
 {
+    /// <summary>
+    /// Constructor that creates mappings between of class DTO and Entity
+    /// </summary>
     public Mapping()
     {
         CreateMap<Bicycle, BicycleDto>().ReverseMap();
